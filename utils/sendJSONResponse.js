1ffcf962c7fda4a0be98e581e3dc257e83aefc87
Mugara250 <mbonyumugara52@gmail.com>
2025-09-25 10:05:03 +0200

    adding the CORS settings

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,7 @@
 export function sendJSONResponse(response, statusCode, payload) {
   response.setHeader("Content-Type", "application/json");
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Methods", "GET");
   response.statusCode = statusCode;
   response.end(JSON.stringify(payload));
 }
